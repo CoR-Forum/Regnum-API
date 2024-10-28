27391d38c65b24b21b6c2c9f6f5476a340f69398
Joshua Treudler <joshua@treudler.net>
2024-10-29 00:56:55 +0100

    Refactor database schema: Remove magnat tables

--- index.js
+++ index.js
@@ -133,24 +133,6 @@ async function initializeDatabase() {
         FOREIGN KEY (user_id) REFERENCES users(id),
         CONSTRAINT user_settings_user_id_unique UNIQUE (user_id)
       );`,
-      `CREATE TABLE IF NOT EXISTS magnat (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id INT NOT NULL,
-        amount INT NOT NULL,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        CONSTRAINT magnat_currency_user_id_unique UNIQUE (user_id)
-      );`,
-      `CREATE TABLE IF NOT EXISTS magnat_transactions (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id INT NOT NULL,
-        magnat_id INT NOT NULL,
-        amount INT NOT NULL,
-        type VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id),
-        FOREIGN KEY (magnat_id) REFERENCES magnat(id)
-      );`,
       `CREATE TABLE IF NOT EXISTS feedback (
         id INT AUTO_INCREMENT PRIMARY KEY,
         type VARCHAR(255) NOT NULL,
