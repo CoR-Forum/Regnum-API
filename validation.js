6b91083aef76e880e740e5807d3c5c0664ed4b85
Joshua Treudler <joshua@treudler.net>
2024-11-01 01:45:31 +0100

    refactor validation.js to import database connection from db.js

--- validation.js
+++ validation.js
@@ -1,16 +1,5 @@
 const validator = require('validator');
-const mysql = require('mysql2/promise');
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    charset: process.env.DB_CHARSET,
-    waitForConnections: true,
-    connectionLimit: process.env.DB_CONN_LIMIT || 10,
-    queueLimit: process.env.DB_QUE_LIMIT || 0
-});
+const { pool } = require('./db'); // Import the pool from db.js
 
 const validateUsername = (username) => {
     const usernameRegex = /^[a-zA-Z0-9]{3,20}$/;
