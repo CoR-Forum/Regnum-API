b3597b3487031cd955ddd6154d2e2509fa1db542
Joshua Treudler <joshua@treudler.net>
2025-01-25 15:25:16 +0100

    feat: add to_discordChannel field to notificationQueueSchema

--- models.js
+++ models.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const notificationQueueSchema = new mongoose.Schema({
     to_email: { type: String },
+    to_discordChannel: { type: String },
     subject: { type: String },
     body: { type: String },
     type: { type: String },
